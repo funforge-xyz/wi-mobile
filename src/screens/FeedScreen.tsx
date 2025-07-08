881b69d0e1629522b18c2fdc311c464d813b0575
funforge-xyz <funforgexyz@gmail.com>
2025-07-08 21:00:39 +0200

    fix: safe area

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -18,7 +18,6 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { FlatList } from 'react-native-gesture-handler';
 import { useAppSelector } from '../hooks/redux';
 import { locationService } from '../services/locationService';
