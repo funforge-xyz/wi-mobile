9ef6bca97091b83dd533746b2ceecba36c7b151d
funforge-xyz <funforgexyz@gmail.com>
2025-07-09 10:17:12 +0200

    fix: color for checkmark

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -51,7 +51,7 @@ const getMessageStatusIcon = (message: Message) => {
           <Ionicons 
             name="checkmark" 
             size={16} 
-            color="#4FC3F7" 
+            color="#ffffff" 
             style={styles.secondCheck}
           />
         </View>
