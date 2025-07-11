2d14377523b1fe483e553e5beded8a922474a498
funforge-xyz <funforgexyz@gmail.com>
2025-07-11 23:31:25 +0200

    fix: ios google sign in

--- app.config.ts
+++ app.config.ts
@@ -22,6 +22,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     "**/*"
   ],
   ios: {
+    googleServicesFile: "./GoogleService-Info.plist",
     supportsTablet: true,
     bundleIdentifier: "com.wichatapp",
     infoPlist: {
